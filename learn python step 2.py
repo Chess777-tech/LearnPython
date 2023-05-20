b6e3999fa57b2ac68f71f45223c2b3b35f1a7e40
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-05-20 00:17:20 -0500

    Add files via upload

---
+++ learn python step 2.py
@@ -0,0 +1,36 @@
+#step 2 
+
+
+#importing 
+import math 
+math.sqrt(9)
+
+import time 
+time.ctime()
+
+#arrays 
+a = [1,2,3,4]
+a 
+a.append(8)
+a
+
+
+#looping through arrays in 2 ways
+for i in range(len(a)):
+    print([i])
+    
+for x in a:
+    print(x)
+
+
+#dictionary 
+d = {'cat':'meow', 'dog':'bark',
+     'bird':'chrip'}
+d['dog'] = 'run'
+
+#if-else
+x = 2
+if x == 1:
+    print(5)
+else:
+    print(1)
